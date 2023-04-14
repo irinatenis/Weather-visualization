3c7e4b121253a364515122e73f7a777b06b6b959
irinatenis <irinatenis78@yahoo.com>
2023-04-13 22:10:20 -0500

    final

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "50e75744c76d87a6a0c7668af6c12154"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "df6020c74efe45fda380c2df4cb09711"
--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -2,4 +2,4 @@
 weather_api_key = "50e75744c76d87a6a0c7668af6c12154"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "df6020c74efe45fda380c2df4cb09711"
